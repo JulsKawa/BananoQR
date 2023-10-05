369236a3c05cb6f78767d7363b1d88b3aa71b3eb
JulsKawa <juvat.2211@gmail.com>
2023-10-05 19:38:09 +0200

    CLI (#3)
    Co-authored-by: Polaire <Polaire>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use qrcode_generator::{QRCodeError, QrCodeEcc};
-use std::io::stdin;
+use clap::Parser;
 
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct BananoQR {
+    amount: f64,
+    address: String,
+    filename: String,
+}
 
 fn main() {
-    let mut input = String::new();
-    println!("Enter the amount of banano you want");
-    
-    stdin().read_line(&mut input).expect("Failed to read line");
+    let args = BananoQR::parse();
 
-    let amount: f64 = input.trim().parse().expect("Please type the raw amount of banano you want");
+    let address = args.address;
+    let amount = args.amount;
+    let image = args.filename;
 
-    let mut input_address = String::new();
-    println!("Enter the banano address you want");
-    stdin().read_line(&mut input_address).expect("Failed to read line");
-
-    let address = input_address.trim();
-
-
-    qrcode_generator::to_png_to_file(format!("ban:{address}?amount={amount}"), QrCodeEcc::Low, 1024, "./image.png").unwrap();
+    qrcode_generator::to_png_to_file(format!("ban:{address}?amount={amount}"), QrCodeEcc::Low, 1024, format!("./{image}.png")).unwrap();
 
 }
